fc8e5f81f450c71693bd52fc135365f1fb55b1ef
Yoline <yolineb@gmail.com>
2022-02-10 02:02:27 +0000

    with condensed variables

--- calculator.py
+++ calculator.py
@@ -12,8 +12,7 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 #num1 = token[1]
 #num2 = token[2]
 #lots of ifs statements
-        #[add, subtr, mult] = operators   operator[0] CALL FUNCTION SOMEHOW
-            #for loop inside operators to see where it is ==, and get idx
+
 
 while True:
     equation = input("> ")
@@ -23,33 +22,24 @@ while True:
     
     if operator == "q":
         break
+
+    num1 = int(tokens[1])
+    if len(tokens) > 2:
+        num2 = int(tokens[2]) 
+
     elif operator == "+":
-        num1 = int(tokens[1])
-        num2 = int(tokens[2])
         print(add(num1,num2))
     elif operator == "-":
-        num1 = int(tokens[1])
-        num2 = int(tokens[2])
         print(subtract(num1, num2))
     elif operator == "*":
-        num1 = int(tokens[1])
-        num2 = int(tokens[2])
         print(multiply(num1, num2))
     elif operator == "/":
-        num1 = int(tokens[1])
-        num2 = int(tokens[2])
         print(divide(num1, num2))
     elif operator == "square":
-        num1 = int(tokens[1])
         print(square(num1))
     elif operator == "cube":
-        num1 = int(tokens[1])
         print(cube(num1))
     elif operator == "pow":
-        num1 = int(tokens[1])
-        num2 = int(tokens[2])
         print(power(num1, num2))
     elif operator  == "%":
-        num1 = int(tokens[1])
-        num2 = int(tokens[2])
         print(mod(num1, num2))
